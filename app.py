dafe90b86bee4a4f77c4873b5c018ecf2c66b59d
ek0212 <ekazarian@g.hmc.edu>
2021-04-23 13:31:15 -0400

    add error message

--- app.py
+++ app.py
@@ -49,19 +49,21 @@ def upload_file():
 	
 	content.save(os.path.join(app.config['UPLOAD_FOLDER'], content.filename))
 	#check image size
-	#load in content and style image
-	content = load_image('./static/image/upload/'+content.filename)
-	#Resize style to match content, makes code easier
-	style = load_image('./static/image/'+ style+'.jpg', shape=content.shape[-2:])
+	if check_image('./static/image/upload/'+content.filename):
+		#load in content and style image
+		content = load_image('./static/image/upload/'+content.filename)
+		#Resize style to match content, makes code easier
+		style = load_image('./static/image/'+ style+'.jpg', shape=content.shape[-2:])
 
-	vgg = model()
-	target = stylize(content,style,vgg)
-	x = im_convert(target)
+		vgg = model()
+		target = stylize(content,style,vgg)
+		x = im_convert(target)
 
-	# app.config['UPLOAD_FOLDER'] would need to be a path to Upload_folder for send_from_directory to work
-	plt.imsave(app.config['UPLOAD_FOLDER']+'/result.png',x)
-	return send_from_directory(app.config['UPLOAD_FOLDER'], "result.png", mimetype='image/png')
-	
+		# app.config['UPLOAD_FOLDER'] would need to be a path to Upload_folder for send_from_directory to work
+		plt.imsave(app.config['UPLOAD_FOLDER']+'/result.png',x)
+		return send_from_directory(app.config['UPLOAD_FOLDER'], "result.png", mimetype='image/png')
+	else:
+		return send_from_directory(app.config['UPLOAD_FOLDER'], "error.png", mimetype='image/png')
 
 @app.route('/result')
 def get_res():
