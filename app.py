2d872379641943ede95a90b1d653370e83d16af4
ek0212 <ekazarian@g.hmc.edu>
2021-04-27 10:17:25 -0400

    code style changes

--- app.py
+++ app.py
@@ -11,27 +11,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 style =""
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-# from flask_ngrok import run_with_ngrok
-# from flask import Flask
-# app = Flask(__name__)
 cors = CORS(app)
-# run_with_ngrok(app)
-
-# @app.route("/")
-# def home():
-#     return "<h1>Running Flask on Google Colab!</h1>"
-
-# @app.route('/stylize', methods=['POST'])
-# def stylize():
-#     input_image = request.files['image']
-#     outputter(input_image, '/tmp/nst/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg', num_iterations=300)
-#     return send_file("result.jpg", mimetype='image/jpg')
-
-# @app.route('/result')
-# def get_res():
-#    return send_file("result.jpg", mimetype='image/jpg')
-
-# app.run()
 
 @app.route("/")
 def home():
@@ -40,7 +20,7 @@ def home():
 
 @app.route("/stylize", methods=['GET','POST'])
 def upload_file():
-	print("Hi from stylize")
+	print("Hi from stylize") #sanity check
 	content = request.files['image']
 	style = request.form.get('style')
 
@@ -50,20 +30,19 @@ def upload_file():
 	content.save(os.path.join(app.config['UPLOAD_FOLDER'], content.filename))
 	#load in content and style image
 	content = load_image('./static/image/upload/'+content.filename)
-	#Resize style to match content, makes code easier
+	#resize style to match content, makes code easier
 	style = load_image('./static/image/'+ style+'.jpg', shape=content.shape[-2:])
 
 	vgg = model()
 	target = stylize(content,style,vgg)
 	x = im_convert(target)
 
-	# app.config['UPLOAD_FOLDER'] would need to be a path to Upload_folder for send_from_directory to work
 	plt.imsave(app.config['UPLOAD_FOLDER']+'/result.png',x)
 	return send_from_directory(app.config['UPLOAD_FOLDER'], "result.png", mimetype='image/png')
 
 @app.route('/result')
 def get_res():
-	print("Hi from result")
+	print("Hi from result") #sanity check
 	return send_from_directory(app.config["UPLOAD_FOLDER"], "result.png", mimetype='image/png')
 							
 
